97d36dd59322d8276baecc9e7f6a8b423209e740
loloopes <gus.lcampos38@gmai.com>
2022-01-05 13:38:02 -0400

    finalize

--- telzir-backend/service/callCalculatorWithContract.js
+++ telzir-backend/service/callCalculatorWithContract.js
@@ -1,20 +1,25 @@
 const prices = require('./prices');
 
 const callCalculatorWithContract = (data) => {
-    const result = data.reduce((acc, cV) => {
-        if (cV.contract === 'FaleMais30') {
-            // return ((cV.min - 30) * prices[cV.oriDest]) > 0 ? [...acc, ((cV.min - 30) * prices[cV.oriDest])] : 0;
-            return [...acc, ((cV.min - 30) * prices[cV.oriDest]) > 0 ? ((cV.min - 30) * prices[cV.oriDest]) : 0]
-        }
-        if (cV.contract === 'Falemais60') {
-            return [...acc, ((cV.min - 60) * prices[cV.oriDest])]
-        }
-        if (cV.contract === 'FaleMais120') {
-            return [...acc, ((cV.min - 120) * prices[cV.oriDest])]
-        }
-        return acc;
-    }, [])
-    return result;
+	const result = data.reduce((acc, cV) => {
+			if (cV.contract === 'FaleMais30') {
+				const minimumValue = cV.min - 30;
+				const total = minimumValue * prices[cV.oriDest];
+				return [...acc, Math.max(total, 0)]
+			}
+			if (cV.contract === 'FaleMais60') {
+				const minimumValue = cV.min - 60;
+				const total = minimumValue * prices[cV.oriDest];
+				return [...acc, Math.max(total, 0)]
+			}
+			if (cV.contract === 'FaleMais120') {
+				const minimumValue = cV.min - 120;
+				const total = minimumValue * prices[cV.oriDest];
+				return [...acc, Math.max(total, 0)]
+			}
+			return acc;
+	}, [])
+	return result;
 }
 
 module.exports = { callCalculatorWithContract };
